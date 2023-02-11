b652d601b19d88dd568cc4019936f52b5f5cb187
Robin Beer <contact@robin-beer.com>
2023-02-11 20:37:07 +0100

    fix og tags for twitter

--- app/head.tsx
+++ app/head.tsx
@@ -9,8 +9,28 @@ export default function Head() {
         content="A simple web app that generates recipes from your leftovers, using GPT-3."
       />
       <meta
+        name="twitter:card"
+        content="summary_large_image"
+      />
+      <meta
+        name="twitter:site"
+        content="@beer_robin"
+      />
+      <meta
+        property="og:url"
+        content="https://artificial.recipes/"
+      />
+      <meta
+        property="og:title"
+        content="Artificial Recipes"
+      />
+      <meta
+        property="og:description"
+        content="A simple web app that generates recipes from your leftovers, using GPT-3."
+      />
+      <meta
         property="og:image"
-        content="https://artificial.recipes/api/static"
+        content="https://artificial.recipes/api/og"
       />
     </>
   );
